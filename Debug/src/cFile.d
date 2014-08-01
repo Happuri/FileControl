e8a2b04bc525b02ee0611bcf3d4c25ddd7b8161e
Happuri <happuri@gmx.com>
2014-08-01 15:48:54 +0200

    Deleted binary files

--- Debug/src/cFile.d
+++
@@ -1,3 +0,0 @@
-src/cFile.d: ../src/cFile.cpp ../src/cFile.h
-
-../src/cFile.h:
--- Debug/src/cManager.d
+++
@@ -1,5 +0,0 @@
-src/cManager.d: ../src/cManager.cpp ../src/cManager.h ../src/cFile.h
-
-../src/cManager.h:
-
-../src/cFile.h:
